c4d953ecc3b46329c2c194ece462f37ef4840c78
Ayushoopp <you@example.com>
2023-02-17 20:23:09 +0530

    Project Done

---
+++ script.js
@@ -0,0 +1,48 @@
+let turn = "X";
+let gameover = false;
+
+const changeTurn = ()=>{
+return turn === "X" ? "O": "X"
+}
+
+const checkWin = () =>{
+    let boxtext = document.getElementsByClassName("boxtext");
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [2,5,8],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== '')){
+            document.querySelector(".info").innerText = boxtext[e[0]].innertext + "Won";
+        } else {
+            gameover = true;
+        }
+     })
+}
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector(".boxtext");
+    element.addEventListener("click", ()=>{
+        if(boxtext.innerText === ''){
+        boxtext.innerText = turn;
+        turn = changeTurn();
+        checkWin();
+        if(!gameover){
+
+            document.getElementsByClassName("info")[0].innerText="Turn for" + turn;
+        }
+        }
+    })
+})
+
+document.getElementById('reset').addEventListener("click", function(){
+    location.reload()
+})
